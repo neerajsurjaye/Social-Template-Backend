4d23d20f01f4078119b0080e8d7bc73fd69099be
neerajsurjaye <neerajsurjaye@gmail.com>
2022-03-07 20:23:37 +0530

    fast commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+const user = require('./models/user')
+
+mongoose.connect('mongodb://127.0.0.1:27017/social-template');
 
 app.get('/', (req, res) => {
     res.send('Listning')
---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    username: String,
+    name: String,
+    profile_pic: String,
+    posts: [{ type: mongoose.Types.ObjectId, ref: 'user_post' }],
+    follows: [{ type: mongoose.Types.ObjectId, ref: 'user' }],
+    password: String,
+    likes: Number
+
+})
+
+const userModel = mongoose.model('user', user);
+
+module.exports = userModel;
